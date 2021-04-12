dec522f990e1682496e8984860bf198e01c593a5
James Mullen <mullenj@vt.edu>
2021-04-12 13:00:40 -0400

    Adding Dr. Loseys Data analysis scripts

---
+++ plot.py
@@ -0,0 +1,62 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# how much time the user was interacting with the joystick
+def interaction_time(data):
+    ah_time = 0
+    for ah in data["ah"]:
+        ah_norm = np.linalg.norm(ah)
+        if ah_norm > 0.01:
+            ah_time += 0.1
+    return ah_time
+
+# Shannon entropy over belief as a function of time
+# i.e., how uncertain the robot about the human's goal as a function of time
+def entropy(data):
+    H = []
+    for b in data["belief"]:
+        H.append(-sum(b * np.log(b + 1e-6)))
+    return np.asarray(H)[-1]
+
+# robot's learned belief in true goal
+def belief(data):
+    bfinal = data["belief"][-1]
+    return bfinal[3]
+
+# amount of time the robot was in critical states
+# my understanding is that the robot stops in these states? So we should want to minimize this?
+def critical_alignment(data):
+    C = np.copy(data["critical"])
+    C /= max(C)
+    time_critical = 0
+    for c in C:
+        if c > 0.8:
+            time_critical += 0.1
+    return time_critical
+
+
+
+TASK = 2
+METHOD = ["A", "B", "C", "D"]
+
+results = []
+
+for method in METHOD:
+
+    results_method = []
+    filename = "data/" + method + str(TASK) + ".pkl"
+    with open(filename, 'rb') as handle:
+        datafull = pickle.load(handle)
+        for data in datafull:
+            ah_time = interaction_time(data)
+            final_entropy = entropy(data)
+            belief_in_g3 = belief(data)
+            time_critical = critical_alignment(data)
+            results_method.append([ah_time, final_entropy, belief_in_g3, time_critical])
+
+    results.append(results_method)
+
+results = np.asarray(results)
+print(results)
---
+++ process.py
@@ -0,0 +1,70 @@
+import pickle
+import numpy as np
+
+
+# takes the raw data and puts it into a dictionary
+# each dictionary entry is a numpy array
+def sort_data(data):
+    D = {"time": [], "q": [], "s": [], "G": [], "ah": [], "astar": [],
+                    "ar": [], "belief": []}
+    for datapoint in data:
+        D["time"].append(datapoint[0])
+        D["q"].append(list(datapoint[1]['q']))
+        D["s"].append(list(datapoint[2]))
+        D["G"].append(list(datapoint[3]))
+        D["ah"].append(list(datapoint[4]))
+        D["astar"].append(list(datapoint[5]))
+        D["ar"].append(list(datapoint[6]))
+        D["belief"].append(list(datapoint[7]))
+    for key in D:
+        D[key] = np.asarray(D[key])
+    D["time"] -= D["time"][0]
+    D["critical"] = get_criticality(D)
+    return D
+
+# helper function to recompute the critical states,
+# should match what was in the code for the user study
+def get_criticality(data):
+    C = []
+    for idx in range(len(data["time"])):
+        belief = data["belief"][idx,:]
+        a_star = data["astar"][idx]
+        G = data["G"][idx]
+        ar = data["ar"][idx,:]
+        s = data["s"][idx,:]
+        C.append(sum([b*(np.linalg.norm(s+ar - goal_x) - np.linalg.norm(s+a_star_x - goal_x)) for b, a_star_x, goal_x in zip(belief, a_star, G)]))
+    return np.asarray(C)
+
+
+
+USERS = [1, 2, 3, 4, 5]
+TASK = [1, 2, 3, 4, 5]
+METHOD = ["A", "B", "C", "D"]
+
+for task in TASK:
+
+    for method in METHOD:
+
+        savename = "data/" + method + str(task) + ".pkl"
+
+        datafull = []
+        for user in USERS:
+            if task < 3:
+                folder = "users/user" + str(user) + "/task" + str(task)
+                filename = "data_method_" + method + ".pkl"
+            elif task == 3:
+                folder = "users/user" + str(user) + "/task" + str(3)
+                filename = "data_method_" + method + "_prior_A.pkl"
+            elif task == 4:
+                folder = "users/user" + str(user) + "/task" + str(3)
+                filename = "data_method_" + method + "_prior_B.pkl"
+            elif task == 5:
+                folder = "users/user" + str(user) + "/task" + str(3)
+                filename = "data_method_" + method + "_prior_C.pkl"
+            trial = pickle.load(open(folder + "/" + filename, "rb"))
+            D = sort_data(trial)
+            D["name"] = folder + "/" + filename
+            datafull.append(D)
+
+        with open(savename, 'wb') as handle:
+            pickle.dump(datafull, handle, protocol=pickle.HIGHEST_PROTOCOL)
